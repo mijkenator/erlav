f3a72b32711f0e2f8dbe7c3621dfa05d7d824dda
Mijkenator <mijkenator@gmail.com>
2023-02-04 17:49:59 -0500

    tests && fail on nullable map

---
+++ test/erlav_ortb1_test.erl
@@ -0,0 +1,27 @@
+-module(erlav_ortb1_test).
+
+-include_lib("eunit/include/eunit.hrl").
+    
+ortb1_test() ->
+    {ok, SchemaJSON1} = file:read_file("test/opnrtb_test1.avsc"),
+    Decoder  = avro:make_simple_decoder(SchemaJSON1, []),
+    SchemaId = erlav_nif:create_encoder(<<"test/opnrtb_test1.avsc">>),
+    {ok, Terms} = file:consult("test/opnrtb_test1.data"),
+
+    lists:foreach(fun(Term1) -> 
+        Re2 = erlav_nif:do_encode(SchemaId, Term1),
+        M = maps:from_list(Decoder(Re2)),
+        compare_maps(Term1, M)
+    end, Terms).
+
+compare_maps(M1, M2) ->
+    KL1 = maps:keys(M1),
+    Event = maps:get(<<"event">>, M1),
+    ?debugFmt("Test for event: ~p ~n", [Event]),
+    lists:foreach(fun(Key) ->
+        V1 = maps:get(Key, M1),
+        V2 = maps:get(Key, M2),
+        ?assertEqual(V1, V2)
+    end, KL1),
+    ok.
+
