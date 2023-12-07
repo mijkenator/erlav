dc081e33989724af501b714d541fbae0f7af155c
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2023-12-07 15:27:28 -0500

    Run formatter on Erlang/C++ files

--- src/erlav_nif.erl
+++ src/erlav_nif.erl
@@ -1,11 +1,11 @@
 -module(erlav_nif).
 
 -export([
-        erlav_init/1,
-        erlav_encode/2,
-        erlav_safe_encode/2,
-        replace_keys/1
-        ]).
+    erlav_init/1,
+    erlav_encode/2,
+    erlav_safe_encode/2,
+    replace_keys/1
+]).
 
 -nifs([erlav_init/1, erlav_encode/2]).
 
@@ -42,30 +42,34 @@ erlav_safe_encode(SchemaId, Data) ->
     erlav_encode(SchemaId, replace_keys(Data)).
 
 replace_keys(Term) when is_map(Term) ->
-    maps:fold(fun (_, [], AccIn)   -> AccIn;
-                  (_, null, AccIn) -> AccIn;
-                  (K, V, AccIn)    -> maps:put(key_to_binary(K), replace_keys(V), AccIn)
-    end, #{}, Term);
-replace_keys([{_,_}|_] = Lst) -> 
+    maps:fold(
+        fun
+            (_, [], AccIn) -> AccIn;
+            (_, null, AccIn) -> AccIn;
+            (K, V, AccIn) -> maps:put(key_to_binary(K), replace_keys(V), AccIn)
+        end,
+        #{},
+        Term
+    );
+replace_keys([{_, _} | _] = Lst) ->
     replace_keys(maps:from_list(Lst));
-replace_keys(L) when is_list(L) -> [replace_keys(Ele) || Ele <- L]; 
-replace_keys(Term) -> value_to_binary(Term).
+replace_keys(L) when is_list(L) -> [replace_keys(Ele) || Ele <- L];
+replace_keys(Term) ->
+    value_to_binary(Term).
 
-value_to_binary([L1|_] = L) when is_integer(L1) -> 
+value_to_binary([L1 | _] = L) when is_integer(L1) ->
     case is_string(L) of
         true -> list_to_binary(L);
         _ -> L
     end;
-value_to_binary(V) -> V.
+value_to_binary(V) ->
+    V.
 
 key_to_binary(A) when is_atom(A) -> atom_to_binary(A);
-key_to_binary([L1|_] = L) when is_integer(L1) -> list_to_binary(L);
+key_to_binary([L1 | _] = L) when is_integer(L1) -> list_to_binary(L);
 key_to_binary(K) -> K.
 
 isprint(X) when X >= 32, X < 127 -> true;
 isprint(_) -> false.
 is_string(List) when is_list(List) -> lists:all(fun isprint/1, List);
 is_string(_) -> false.
-
-    
-
